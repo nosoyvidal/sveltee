a072205e5ba86221c0e8ba4e7f171ad2b1a747c4
batman <norkio@gmail.com>
2025-03-27 17:44:32 -0500

    Subiendo el proyecto inicial

---
+++ paises-sur-america/app-paises/src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+  import Paises from "./lib/paises.svelte";
+  const lista = ["colombia", "venezuela", "ecuador", "peru", "brasil"];
+</script>
+
+<div class="main">
+  {#each lista as pais}
+    <div>
+      <Paises {pais} />
+    </div>
+  {:else}
+    No existen elementos
+  {/each}
+</div>
+
+<svelte:head>
+  <title>Paises de suramerica</title>
+</svelte:head>
+
+<style>
+  .main {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 10px;
+    grid-auto-rows: minmax(100px, auto);
+  }
+</style>
