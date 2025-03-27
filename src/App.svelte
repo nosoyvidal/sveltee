8cb9986d726a3241ab667926da8b94027682cccd
batman <norkio@gmail.com>
2025-03-27 17:27:50 -0500

    Primer commit - Proyecto inicializado

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  // Importamos el componente parImpar.svelte y lo llamamos ParImpar
+  import ParImpar from "./lib/parImpar.svelte";
+  let valor = 0;
+
+  // Escuchamos el evento `input`
+  const listenInput = (event) => (valor = event.target.value);
+</script>
+
+<main>
+  <!-- Llamamos el componente que inicialmente procesa el valor de 0 -->
+  <ParImpar numero={valor} />
+
+  <!-- En el elemento input se coloca en value la variable valor que vale 0 -->
+  <!-- También se llama el evento on:input para que reaccione al ingresar los números -->
+  <input value={valor} on:input={listenInput} />
+</main>
---
+++ src/lib/parImpar.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let numero; //Exportamos la variable numero para poder llamarla en App.svelte
+    let resultado; //Variable local
+
+    $: if (numero === 0)
+        //Se coloca el símbolo $: para generar reactividad en el if para resultado
+        resultado = "neutro";
+    else if (numero % 2 === 0) resultado = "par";
+    else resultado = "impar";
+</script>
+
+<!-- Las variables las llamamos entre llaves -->
+<p>El número {numero} es {resultado}</p>
+
+<style>
+    p {
+        color: rgb(34, 5, 112);
+        font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+        font-size: 25px;
+    }
+</style>
